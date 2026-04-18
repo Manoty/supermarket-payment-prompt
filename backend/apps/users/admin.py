d760e3acd95989a12ff5b444a040d4d7ae647900
Kevin Manoti <kevinnoty21@gmail.com>
2026-04-18 19:44:32 +0300

    update user admin config

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,26 @@
-from django.contrib import admin
+# backend/apps/users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('phone_number', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('phone_number',)
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('phone_number', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('date_joined', 'last_login')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
+    readonly_fields = ('date_joined', 'last_login')
